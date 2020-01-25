686375ad765d6c68dd3abd01c5ef30281b595cba
baptiste3672 <baptiste.mergey@gmail.com>
2020-01-25 13:29:33 +0100

    Ajout d'une 3eme page main

---
+++ main2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="CSS.css"> 
+        <title>Main</title>
+    </head>
+
+    <body>
+        <div class="menu">
+            <ul>
+                <li><a href="">Menu 1</a></li>
+                <li><a href="">Menu 2</a></li>
+                <li><a href="">Menu 3</a></li>
+            </ul>
+        </div>
+          
+    	<p>Bonjour, je suis un <em>paragraphe</em> de texte !</p>
+    	<ul>
+    		<li style="color: red">Presentation</li>
+    		<li style="color: blue">PHP</li>
+    		<li style="color: orange">MySQL</li>
+    	</ul>
+        <p>Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.Un paragraphe est entré ici.ahjsbhajsbhjzchbaxvhkzebdkjqbgzxbqlxqzqx</p>
+    </body>
+</html>
