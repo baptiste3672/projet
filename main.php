4d653f170c61d4a5a15651f10fae6c76f1c70748
baptiste3672 <baptiste.mergey@gmail.com>
2020-01-25 13:10:35 +0100

    Avancement création d'un début de menu version 0.0.3

--- main.php
+++ main.php
@@ -2,11 +2,20 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="CSS.css"> 
         <title>Main</title>
     </head>
 
     <body>
-    	<h1>Titre</h1>
+        <div class="menu">
+            <ul>
+                <li><a href="">Menu 1</a></li>
+                <li><a href="">Menu 2</a></li>
+                <li><a href="">Menu 3</a></li>
+            </ul>
+        </div>
+
+    	<h3>Apprentissage du PHP</h3>
     	<?php 
             $age = 12;
             if ($age < 12) {
